d3e8febf5fd69c8b320c0c639a218306c4c0b22b
joak990 <joakhaidar@gmail.com>
2023-08-22 13:48:34 -0300

    whatsapp resolve

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
         <div>
             <div className="flex justify-end mt-8 ">
                 <a href="https://api.whatsapp.com/send?phone=541131666991" target="_blank" rel="noopener noreferrer">
-                    <img src={wplogo} alt="WhatsApp" className="h-12  md:h-16 md:w-16 fixed bottom-4  right-2" />
+                    <img src={wplogo} alt="WhatsApp" className="h-12  md:h-16 md:w-16 fixed bottom-4  right-2 z-50" />
                 </a>
             </div>
             <Navbar />
