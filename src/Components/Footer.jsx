dce3feb72a6c4ae67bbbf6f64a582522f2cc5b81
patrickmurayari <patrickmurayari03@gmail.com>
2024-09-13 18:50:18 -0300

    modified prueba

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
     return (
         <footer className="bg-gray-800 text-white text-center py-0.5">
-            <p>Diseño Web &copy; Pauin 2023</p>
+            <p>Diseño supper web &copy; Pauin 2023</p>
         </footer>
     )
 }
